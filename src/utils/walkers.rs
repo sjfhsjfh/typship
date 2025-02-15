6a85b3c10bd302275b5037903ac4fc10a6bef3ab
sjfhsjfh <sjfhsjfh@qq.com>
2025-02-16 00:07:32 +0800

    refactor: two walkers

---
+++ src/utils/walkers.rs
@@ -0,0 +1,50 @@
+use anyhow::Result;
+use glob::Pattern;
+use ignore::{Walk, WalkBuilder};
+use std::path::Path;
+
+use super::read_manifest;
+
+pub fn walker_publish(root: &Path) -> Walk {
+    WalkBuilder::new(root)
+        .standard_filters(true)
+        .add_custom_ignore_filename(".typstignore")
+        .build()
+}
+
+pub fn walker_install(
+    root: &Path,
+) -> Result<Vec<std::result::Result<ignore::DirEntry, ignore::Error>>> {
+    let excludes = read_manifest(root)?
+        .package
+        .exclude
+        .into_iter()
+        .map(|s| Pattern::new(&s))
+        .collect::<Vec<_>>();
+    let mut ok_excludes = vec![];
+    for pat in &excludes {
+        match pat {
+            Ok(p) => ok_excludes.push(p),
+            Err(e) => {
+                return Err(anyhow::anyhow!("Invalid pattern: {}", e));
+            }
+        }
+    }
+    Ok(WalkBuilder::new(root)
+        .standard_filters(true)
+        .add_custom_ignore_filename(".typstignore")
+        .build()
+        .filter(|entry| {
+            if let Ok(path) = entry {
+                return ok_excludes.iter().any(|p| p.matches_path(path.path()));
+            }
+            false
+        })
+        .map(|entry| {
+            if let Ok(path) = entry {
+                return Ok(path);
+            }
+            Err(entry.unwrap_err())
+        })
+        .collect())
+}
